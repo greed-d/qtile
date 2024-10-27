71a1d2f737d2226b24950627ebb838eb4f673316
greed-d <deekshit.bti@gmail.com>
2024-10-28 00:51:46 +0545

    `chore` : add all the panel configs

--- configs/keybinds.py
+++ configs/keybinds.py
@@ -1,11 +1,16 @@
 from libqtile import layout
-from libqtile.config import Click, Drag, Group, Key, Match
+from libqtile.config import (
+    Click,
+    Drag,
+    Key,
+    KeyChord,
+)
 from libqtile.lazy import lazy
 from libqtile.widget import backlight
 from qtile_extras.popup.templates.mpris2 import COMPACT_LAYOUT, DEFAULT_LAYOUT
 
 mod = "mod4"
-terminal = "kitty"
+terminal = "ghostty"
 file_manager = "thunar"
 
 mouse = [
@@ -21,6 +26,8 @@ mouse = [
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 keys = [
+    # Key([mod], "x", lazy.group["scratchpad"].dropdown_toggle("term")),
+    # Key([], "F12", lazy.group["scratchpad"].dropdown_toggle("bashtop")),
     Key(
         [],
         "XF86MonBrightnessUp",
@@ -67,12 +74,7 @@ keys = [
         "m",
         lazy.spawn("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
     ),
-    Key([mod], "p", lazy.widget["mpris2"].show_popup(DEFAULT_LAYOUT)),
-    # Key(
-    #    [mod, "alt"],
-    #     "s",
-    #     lazy.spawn(),
-    # ),
+    # Key([mod], "p", lazy.widget["mpris2"].toggle_player()),
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
@@ -83,30 +85,47 @@ keys = [
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key(
-        [mod, "control"],
+        [mod, "shift"],
         "h",
         lazy.layout.shuffle_left(),
         desc="Move window to the left",
     ),
     Key(
-        [mod, "control"],
+        [mod, "shift"],
         "l",
         lazy.layout.shuffle_right(),
         desc="Move window to the right",
     ),
-    Key([mod, "control"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "control"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    Key([mod], "backslash", lazy.layout.max(), desc="Move window down"),
+    Key(
+        [mod, "control"],
+        "l",
+        lazy.spawn("betterlockscreen -l", shell=True),
+        desc="Move window to the left",
+    ),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "shift"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "shift"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod, "mod1"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "mod1"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "mod1"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "mod1"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod], "backslash", lazy.window.toggle_maximize(), desc="Mimic monocle mode"),
     # Toggle between split and unsplit sides of stack. Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
+    KeyChord(
+        [mod],
+        "z",
+        [
+            Key([], "g", lazy.layout.grow()),
+            Key([], "s", lazy.layout.shrink()),
+            Key([], "n", lazy.layout.normalize()),
+            Key([], "m", lazy.layout.maximize()),
+        ],
+        mode=True,
+        name="Windows",
+    ),  # multiple stack panes
     Key(
         [mod, "shift"],
         "Return",
@@ -120,13 +139,40 @@ keys = [
         [mod],
         "t",
         lazy.spawn("kill -s USR1 $(pidof deadd-notification-center)", shell=True),
-        desc="Launch File Manager",
+        desc="Show notification center",
     ),
     Key(
         [],
         "print",
-        lazy.spawn("maim ~/Pictures/Screenshots/$(date +%s).png ", shell=True),
+        lazy.spawn(
+            "/home/greed/.local/scripts/screenshot.sh --screenshot",
+            shell=True,
+        ),
         desc="Launch File Manager",
+    ),
+    Key(
+        [mod, "shift"],
+        "s",
+        lazy.spawn(
+            "/home/greed/.local/scripts/screenshot.sh --screen-selection-buffer",
+            shell=True,
+        ),
+    ),
+    Key(
+        [mod, "shift"],
+        "q",
+        lazy.spawn(
+            "shutdown now",
+            shell=True,
+        ),
+    ),
+    Key(
+        [mod, "mod1"],
+        "s",
+        lazy.spawn(
+            "maim | xclip -selection clipboard -t image/png && notify-send 'Screenshot' 'Screenshot copied to buffer'",
+            shell=True,
+        ),
     ),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
@@ -148,7 +194,10 @@ keys = [
     Key(
         [mod],
         "space",
-        lazy.spawn("rofi -show drun"),
+        lazy.spawn(
+            "pkill rofi || rofi -show drun -theme ~/.config/rofi/catppuccin.qtile.rasi",
+            shell=True,
+        ),
         desc="Spawn a command using a prompt widget",
     ),
 ]
--- configs/panel.py
+++ configs/panel.py
@@ -1,15 +1,28 @@
 from libqtile import bar, widget
+import os
 from libqtile.lazy import lazy
 from libqtile.config import Screen
 from qtile_extras import widget as ex_widget
-from qtile_extras.popup.templates.mpris2 import DEFAULT_LAYOUT
-from qtile_extras.widget.decorations import RectDecoration, BorderDecoration
+from configs.components.power_menu import show_power_menu
+
+# from qtile_extras.popup.templates.mpris2 import DEFAULT_LAYOUT
+
+from configs.components.mpris2_popup import DEFAULT_LAYOUT
+from qtile_extras.widget.decorations import RectDecoration
 from qtile_extras.widget.groupbox2 import GroupBoxRule
-from configs.window_map import CustomWindowName
-from configs.volume_widget import VOLUME_NOTIFICATION
+
+from configs.components.window_map import CustomWindowName
+
+# from configs.window_map import CustomWindowName
+
+# from configs.volume_widget import VOLUME_NOTIFICATION
+
+# from configs.mpris_custom import CustomMprisWidget
 
 
-def draw_bottom_line_margin(line_colour="fff", line_width=1, margin=0):
+# Dictionary mapping player names to their icons
+# You can customize these icons based on your needs
+def draw_bottom_line_margin(line_colour="fff", line_width=2, margin=0):
     def _wrapper(box):
         ctx = box.drawer.ctx
         ctx.set_line_width(line_width)
@@ -25,13 +38,13 @@ def draw_bottom_line_margin(line_colour="fff", line_width=1, margin=0):
 ws_decoration = {
     "decorations": [
         RectDecoration(
-            colour="#1e1e2e", radius=6, filled=True, padding_y=5, group=True
-        ),
-        BorderDecoration(
-            colour="#fab387",
-            border_width=1,
-            radius=6,
+            colour="#242438",
+            radius=9,
+            filled=True,
             padding_y=5,
+            group=True,
+            line_colour="#F5C2E7",
+            line_width=1,
         ),
     ],
     "padding": 10,
@@ -39,10 +52,11 @@ ws_decoration = {
 layout_group = {
     "decorations": [
         RectDecoration(
-            colour="#1e1e2e",
-            radius=6,
+            colour="#242438",
+            radius=9,
             filled=True,
             padding_y=5,
+            # margin_x=7,
             group=True,
         )
     ],
@@ -51,29 +65,72 @@ layout_group = {
 
 decoration_group = {
     "decorations": [
-        RectDecoration(colour="#1e1e2e", radius=6, filled=True, padding_y=5, group=True)
+        RectDecoration(colour="#242438", radius=9, filled=True, padding_y=5, group=True)
     ],
     "padding": 10,
 }
-
 screens = [
     Screen(
         top=bar.Bar(
             [
                 widget.Spacer(length=7),
-                CustomWindowName(foreground="#f2cdcd", **decoration_group),
+                ex_widget.CurrentLayoutIcon(
+                    scale=0.4,
+                    width=26,
+                    **decoration_group,
+                ),
+                # ex_widget.CurrentLayout(
+                #     foreground="#a6e3a1", padding=10, **layout_group
+                # ),
+                widget.Spacer(length=7),
+                CustomWindowName(
+                    foreground="#f2cdcd", max_chars=30, **decoration_group
+                ),
+                # ex_widget.WindowName(width=200, **decoration_group),
                 widget.Spacer(length=7),
                 # CustomMpris2(
-                #     foreground="#cba6f7",  # Customize foreground color
-                #     max_chars=30,  # Adjust character limit for display
-                #     **decoration_group,  # Apply decorations if needed
+                #     name="mpris2",
+                #     foreground="#cba6f7",
+                #     width=200,
+                #     max_chars=30,
+                #     display_metadata=["xesam:title"],  # Only show title
+                #     stopped_text="No player",
+                #     popup_layout=DEFAULT_LAYOUT,
+                #     popup_show_args={
+                #         "relative_to": 2,
+                #         "relative_to_bar": True,
+                #         "x": -700,
+                #     },
+                #     mouse_callbacks={"Button1": lazy.widget["mpris2"].toggle_player()},
+                #     **decoration_group,
+                # ),
+                # CustomMprisWidget(
+                #     foreground="#cba6f7",
+                #     max_chars=30,
+                #     stopped_text="No music",
+                #     popup_layout=DEFAULT_LAYOUT,
+                #     popup_show_args={
+                #         "relative_to": 2,
+                #         "relative_to_bar": True,
+                #         "x": -700,
+                #     },
+                #     **decoration_group,
                 # ),
                 ex_widget.Mpris2(
                     foreground="#cba6f7",
                     max_chars=30,
                     paused_text="   {track}",
-                    format="{xesam:player} {xesam:title}",
+                    format="{xesam:title}",
+                    # format=lambda text, data: f"{get_player_icon(data.get('qtile:player', ''))} {data.get('xesam:title', '')}",
                     popup_layout=DEFAULT_LAYOUT,
+                    popup_show_args={
+                        "relative_to": 2,
+                        "relative_to_bar": True,
+                        "x": -700,
+                        "y": 0,
+                    },
+                    stopped_text="󰝚  Media",
+                    mouse_callbacks={"Button1": lazy.widget["mpris2"].toggle_player()},
                     **decoration_group,
                 ),
                 widget.Spacer(length=7),
@@ -84,109 +141,142 @@ screens = [
                 ),
                 widget.Spacer(length=7),
                 ex_widget.Systray(margin_x=40, **decoration_group),
+                widget.Spacer(length=7),
+                ex_widget.CheckUpdates(
+                    colour_have_updates="#fab387",
+                    colour_no_updates="#cba6f7",
+                    no_update_string="   Fully Updated!",
+                    distro="Arch_checkupdates",
+                    display_format="   {updates} Updates",
+                    **decoration_group,
+                ),
                 widget.Spacer(),
                 ex_widget.GroupBox2(
-                    padding_x=10,
+                    padding_x=12,
                     fontsize=16,
-                    highlight_method="border",
+                    highlight_method="line",
                     width=1,
                     rules=[
                         GroupBoxRule(
                             text_colour="#a6e3a1",
                             custom_draw=draw_bottom_line_margin(
-                                line_colour="#a6e3a1", line_width=3, margin=10
+                                line_colour="#a6e3a1",
+                                # line_width=2,
+                                margin=10,
                             ),
                         ).when(screen=GroupBoxRule.SCREEN_THIS),
                         GroupBoxRule(text_colour="#89b4fa").when(occupied=True),
+                        # GroupBoxRule(text_colour="#ff00ff").when(
+                        #     group_name="3", occupied=True
+                        # ),
                         GroupBoxRule(text_colour="#585b70"),
                     ],
                     **ws_decoration,
                 ),
+                widget.Spacer(length=7),
                 widget.Chord(
                     chords_colors={
                         "launch": ("#ff0000", "#ffffff"),
                     },
                     name_transform=lambda name: name.upper(),
-                ),
-                widget.Spacer(),
-                ex_widget.CurrentLayoutIcon(
-                    foreground="#a6e3a1",
-                    padding=8,
-                    scale=0.4,
-                    margin=0,
-                    **layout_group,
-                ),
-                ex_widget.CurrentLayout(
-                    foreground="#a6e3a1", padding=8, **layout_group
-                ),
-                widget.Spacer(length=7),
-                ex_widget.CheckUpdates(
-                    colour_have_updates="#f38ba8",
-                    colour_no_updates="#cba6f7",
-                    no_update_string="Fully Updated!",
-                    distro="Arch_checkupdates",
-                    display_format="   {updates} Updates",
                     **decoration_group,
                 ),
-                widget.Spacer(length=7),
+                widget.Spacer(),
+                # ex_widget.PulseVolume(emoji=True, hide_after=0.5, **decoration_group),
                 ex_widget.Memory(
                     foreground="#f9e2af",
                     format="  {MemPercent}%",
                     **decoration_group,
                 ),
                 widget.Spacer(length=7),
+                # ex_widget.IWD(show_image=True, **decoration_group),
+                ex_widget.Wlan(
+                    foreground="#cba6f7",
+                    scale=0.5,
+                    show_image=True,
+                    show_text=True,
+                    format="󰤨   {essid} ({percent:2.0%})",
+                    **decoration_group,
+                ),
+                widget.Spacer(length=7),
                 ex_widget.PulseVolumeExtra(
+                    mode="both",
+                    icon_size=20,
                     bar_colour_normal="#a6e3a1",
                     bar_colour_loud="#f38ba8",
                     bar_colour_high="#fab387",
+                    bar_text_foreground="#585b70",
                     bar_width=70,
-                    bar_height=27,
-                    **decoration_group,
-                    # decorations=[
-                    #     RectDecoration(
-                    #         colour="#1e1e2e",
-                    #         radius=6,
-                    #         filled=True,
-                    #         padding_y=5,
-                    #         group=True,
-                    #     )
-                    # ],
-                ),
-                widget.Spacer(length=7),
-                ex_widget.Wlan(
-                    foreground="#cba6f7",
-                    show_image=True,
-                    show_text=True,
+                    bar_height=17,
+                    hide_after=1,
+                    theme_path="~/.config/qtile/audio-icon-svg",
                     **decoration_group,
                 ),
+                # ex_widget.PulseVolumeExtra(
+                #     popup_layout=VOLUME_NOTIFICATION,
+                #     # mouse_callbacks={"Button1": lazy.widget[""].toggle_player()},
+                # ),
                 widget.Spacer(length=7),
+                ex_widget.BatteryIcon(
+                    theme_path="~/.config/qtile/battery-icons-svg",
+                    foreground="#f5c2e7",
+                    width=47,
+                    scale=2.5,
+                    # padding=5,
+                    **layout_group,
+                ),
                 ex_widget.Battery(
+                    # padding=10,
+                    width=45,
+                    foreground="#f5c2e7",
                     format="{percent:2.0%}",
+                    **layout_group,
+                ),
+                widget.Spacer(length=7),
+                ex_widget.Backlight(
+                    foreground="#f9e2af",
+                    backlight_name="amdgpu_bl1",
+                    change_command="brightnessctl set {0}%",
+                    format="   {percent:2.0%}",
                     **decoration_group,
                 ),
                 widget.Spacer(length=7),
+                # ex_widget.BrightnessControl(
+                #     device="/sys/class/backlight/amdgpu_bl1",
+                #     **decoration_group,
+                # ),
+                # widget.Spacer(length=7),
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
                 ex_widget.Clock(
                     foreground="#fab387", format="󰥔  %H:%M:%S", **decoration_group
                 ),
                 widget.Spacer(length=7),
-                ex_widget.ScriptExit(
-                    default_text="[ 󰐥 ]",
-                    countdown_format="[Loggin out in : {} secs]",
+                # ex_widget.ScriptExit(
+                #     default_text="[ 󰐥 ]",
+                #     countdown_format="[Loggin out in : {} secs]",
+                #     mouse_callbacks={"Button1": lazy.function(show_power_menu)},
+                #     **decoration_group,
+                # ),
+                ex_widget.Image(
+                    filename="~/.config/qtile/resorces/power-icons/power.svg",
+                    mask=True,
+                    colour="#ffffff",
+                    scale=True,
+                    mouse_callbacks={"Button1": lazy.function(show_power_menu)},
                     **decoration_group,
                 ),
                 widget.Spacer(length=7),
             ],
             43,
-            background="#45475a",
+            background="#3B3D4D",
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-            margin=7,
+            margin=[7, 12, 0, 10],
         ),
         # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
         # By default we handle these events delayed to already improve performance, however your system might still be struggling
         # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
-        # x11_drag_polling_rate = 60,
+        # x11_drag_polling_rate=60,
     ),
 ]
